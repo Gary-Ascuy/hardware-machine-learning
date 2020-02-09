372c50f432c90f5765aede6be43236c06ef55644
Gary Ascuy <gary.ascuy@gmail.com>
2020-02-09 14:09:51 -0500

    Cleaning code

--- golang/main.go
+++ golang/main.go
@@ -1,5 +1,5 @@
-// Author: Gary Ascuy
-// Example: go run main.go --image assets/orange.png
+// Author: Gary Ascuy <gary.ascuy@gmail.com>
+// Example: go run main.go --histo --image assets/orange.png
 // Yellow = {255, 255, 0}, Red = {255, 0, 0}
 
 package main
@@ -8,13 +8,14 @@ import (
   "os"
   "fmt"
   "flag"
+  "math"
   "image"
   _ "image/gif"
   _ "image/jpeg"
   _ "image/png"
 )
 
-// Command Line Interface - Inputs 
+// Command Line Interface - Inputs
 var imagePath = flag.String("image", "./assets/orange.png", "Path to image (gif, jpeg, png)")
 var histogramFlag = flag.Bool("histo", false, "Show image histogram")
 
@@ -29,13 +30,11 @@ func main() {
     if img, _, imgErr := image.Decode(infile); imgErr == nil {
       fmt.Println(", Ready !!!")
       bounds := img.Bounds()
-      
-      // Histogram 
-      if *histogramFlag {
-        Histogram(bounds, img)
-      }
 
-      // Predict 
+      // Histogram
+      if *histogramFlag { Histogram(bounds, img) }
+
+      // Predict
       Predict(bounds, img)
     } else { flag.Parse() }
   } else { flag.Parse() }
@@ -43,31 +42,26 @@ func main() {
 
 // Prints Predict information
 func Predict(bounds image.Rectangle, img image.Image) {
-  var yellow uint32 = 0 
+  var yellow uint32 = 0
   var red uint32 = 0
   var half uint32 = 32767
 
   for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
     for x := bounds.Min.X; x < bounds.Max.X; x++ {
       r, g, b, _ := img.At(x, y).RGBA()
+      if r == g && g == b { continue }
 
-      if r == g && g == b {} else { // Remore White
-        if r > half && g > half && b < half { yellow++ }
-        if r > half && g < half && b < half { red++ }
-      }
+      if r > half && g > half && b < half { yellow++ }
+      if r > half && g < half && b < half { red++ }
     }
   }
 
   redMean := float64(red) / (float64(red) + float64(yellow))
-  fmt.Println("\n{ RED , YELLOW } = {", red, ",", yellow, "}, Mean =", redMean)
-  if redMean > 0.5 {
-    fmt.Println("Predict: It is an 'Strawberry'")
-  } else {
-    fmt.Println("Predict: It is an 'Orange'")
-  }
+  fmt.Println("\n\n{ RED =", red, ", YELLOW =", yellow, "}, RED =", math.Round(redMean * 100), "%")
+  fmt.Println("Prediction: It is an", TernaryOperator(redMean > 0.5, "STRAWBERRY", "ORANGE"), "!\n")
 }
 
-// Prints Image Histogram 
+// Prints Image Histogram
 func Histogram(bounds image.Rectangle, img image.Image) {
   var histogram [16][4]int
   for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
@@ -85,4 +79,9 @@ func Histogram(bounds image.Rectangle, img image.Image) {
   for i, x := range histogram {
     fmt.Printf("0x%04x-0x%04x: %6d %6d %6d %6d\n", i<<12, (i+1)<<12-1, x[0], x[1], x[2], x[3])
   }
+}
+
+func TernaryOperator(expresion bool, a string, b string) string {
+  if (expresion) { return a }
+  return b
 }
